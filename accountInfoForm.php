30118a62e13af77503e0946c1a879e9bee46946e
Mohammed Ayyad <mohammedayyad25@gmail.com>
2016-11-28 23:36:56 +0200

    Create accountInfoForm.php

---
+++ accountInfoForm.php
@@ -0,0 +1,13 @@
+<?php
+if($_POST)
+{
+    $accountid=$_POST['accountid'];
+    $type=$_POST['type'];
+    $accountpassword=$_POST['accountpassword'];
+    echo "success";
+}else 
+{ 
+    
+}
+
+?>
